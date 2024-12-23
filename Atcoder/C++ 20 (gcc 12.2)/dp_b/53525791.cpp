ff342ae3ac14163b5d23bccb95ab0987406a85de
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-12-23 11:28:43 +0530

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/dp_b/53525791.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define lli long long int 
+#define endl '\n'
+using pii=pair<lli,lli>;
+#define MP make_pair
+#define F first
+
+#define S second
+#define all(o) o.begin(),o.end()
+lli M=1e9+7;
+lli INF=1e17;
+#define state pair<lli,pii>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define deb(x) cout << #x << "=" << x << "\n"
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define pb push_back
+lli n,w,m,k,pb1,ps,t;
+#define mid (l+r)/2
+
+vector<set<lli>> st;
+vector<vector<lli>> g,indi;
+vector<lli> par,size1,distinct,val;
+vector<pii> edges;
+
+
+
+void print(vector<lli> &arr){
+  for(auto a:arr){
+    cout<<a<<" ";
+  }
+  cout<<endl;
+}
+
+
+multiset<lli> mt;
+vector<lli> fin;
+ lli found=0,r;
+lli nb,ns,nc,hb,hc,hs,rb,rs,rc;
+
+template <typename T> T bin_pow(T b, T p, T m){T x = 1;while(p){if(p&1)x=(x*b)%m;b=(b*b)%m;p=p>>1;}return x;}
+
+#undef mid
+  string str;
+
+vector<lli> arr;
+vector<lli> primes;
+bool is_prime[10001];
+
+
+lli msb(lli x){
+  lli temp=x;
+  lli count=0;
+  while(temp){
+     count++;
+     temp/=2;
+  }
+  return count;
+}
+
+lli dp[100100],min_[100100];
+bool done[100100];
+
+void solve(){
+ cin>>n>>k;
+arr.resize(n);
+for(int i=0;i<n;i++){
+  cin>>arr[i];
+}
+
+for(int i=0;i<100010;i++){
+   dp[i]=1e17;
+}
+
+for(int i=n;i>=0;i--){
+  if(i==n-1){
+    dp[i]=0;
+    continue;
+  }
+  for(int j=1;i+j<n && j<=k;j++){
+    dp[i]=min({dp[i],abs(arr[i]-arr[i+j])+dp[i+j]});
+  }
+}
+
+cout<<dp[0]<<endl;
+
+}
+
+
+
+
+
+int main() {
+   IOS
+  // for(int i=0;i<10001;i++){
+  //   is_prime[i]=true;
+  // }
+
+  // for(int i=2;i<=10000;i++){
+  //   if(is_prime[i]){
+  //     primes.pb(i);
+  //     for(int j=i*i;j<=10000;j+=i){
+  //       is_prime[j]=false;
+  //     }
+  //   }
+  // }
+  // cout<<primes.size()<<endl;
+
+  // cin>>t;
+  t=1;
+    while(t--)
+     solve();
+
+
+}
