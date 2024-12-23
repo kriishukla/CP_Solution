12c222d9d7e15c0d13ee0e8ce92c23f8e7df97cb
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-12-23 11:26:07 +0530

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc362_b/55520886.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lli long long int 
+#define endl '\n'
+using pii=pair<lli,lli>;
+#define MP make_pair
+#define F first
+#define ld long double
+#define S second
+#define all(o) o.begin(),o.end()
+lli M=1e9+7;
+using mll = map<lli,lli>;
+using vi = vector<lli>;
+using vvi = vector<vector<lli>>;
+using vpii = vector<pii>;
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define mem(a,b) memset(a,b,sizeof(a))
+#define state pair<lli,pii>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define deb(x) cout << #x << "=" << x << "\n"
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << "\n"
+#define pb push_back
+#define mid (l+r)/2
+#define lb lower_bound
+#define ub upper_bound
+template <typename T> T gcd(T a, T b){return (b==0)?a:gcd(b,a%b);}
+template <typename T> T bin_pow(T b, T p, T m){T x = 1;while(p){if(p&1)x=(x*b)%m;b=(b*b)%m;p=p>>1;}return x;}
+//---- Debugger ---- //
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+lli fact[1000100],inv_fact[1000100];
+lli nCr(lli n,lli r){if(r>n || n<0 ||r<0)return 0;lli d=(inv_fact[r]*inv_fact[n-r])%M;  lli f=(fact[n]*d)%M;return f;}
+lli nPr(lli n,lli r){return (nCr(n,r)*fact[r])%M;}
+lli n,m,x,k,tt,q,p,y,z,h,w;
+lli lcm(lli a,lli b){
+    return (a*b/__gcd(a,b));
+}
+
+vi cost,dd,cntk,brr,crr,hh;
+
+#undef mid
+ vi prime,primes;
+  
+ vi arr,pref1;
+string str;
+lli dis(lli x1,lli y1,lli x2,lli y2){
+  return (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+}
+
+void solve(){
+ lli xa,xb,xc,ya,yb,yc;
+ cin>>xa>>ya;
+ cin>>xb>>yb;
+ cin>>xc>>yc;
+ vi st;
+ st.pb(dis(xa,ya,xb,yb));
+ st.pb(dis(xa,ya,xc,yc));
+ st.pb(dis(xb,yb,xc,yc));
+ sort(all(st));
+ if(st[2]==st[0]+st[1]){
+  yes;
+ }
+ else{
+  no;
+ }
+}
+
+int main() {
+    IOS
+  //  fact[0]=1;
+  //  for(int i=1;i<=1000000;i++){
+  //    fact[i]=(i*fact[i-1])%M;
+  //  }
+  //  for(int i=0;i<=1000000;i++){
+  //   inv_fact[i]=bin_pow<lli>(fact[i],M-2,M);
+  //  }
+  //  deb(nCr(5,2));
+
+    // cin>>tt;
+    tt=1;
+    while(tt--)
+     solve();
+    
+}
